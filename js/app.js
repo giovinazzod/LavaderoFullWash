74776f4313ec8ff9c8cf42b3bf436e1b3c147237
Diego Giovinazzo <dgiovinazzo@mpf.gov.ar>
2024-07-09 20:59:18 -0300

    Subimos modal para editar turno

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ function mostrarTurnos(turnos) {
             <td>${turno.servicio_descripcion}</td>
             <td>${turno.servicio_costo}</td>
             <td>
-                <a href="#" onclick="editarTurno(${turno.id})" title="Editar">
+                <a href="#" onclick="editarTurno(this, ${turno.id})" title="Editar">
                     <i class="fas fa-edit fa-lg" style="color: #011638;"></i>
                 </a>
                 <a href="#" onclick="eliminarTurno(${turno.id})" title="Eliminar">
@@ -100,7 +100,7 @@ function mostrarTurnos(turnos) {
     });
 }
 
-function editarTurno(id) {
+function updateTurnoTest(id) {
     let turnos = JSON.parse(localStorage.getItem('turnos'));
     //se utiliza el metodo find para poder asegurarnos que exista un turno con el id que queremos editar.
     let turnoAActualizar = turnos.find(turno => turno.id === id);
@@ -176,6 +176,173 @@ function crearTurno() {
 
     fetch(BASEURL + '/api/crearTurno', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(data => {
+            Swal.fire({
+                icon: 'success',
+                title: '¡Formulario recibido!',
+                text: 'Gracias por agendar tu turno.',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar',
+            })
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al crear el turno',
+                text: 'Por favor, intente nuevamente',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar',
+            })
+        });
+}
+
+function editarTurno(button, id) {
+    // Obtengo la fila de la tabla del botón "Modificar" seleccionado
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName("td");
+
+    // Obtengo los datos de la fila
+    var fecha = cells[0].innerText;
+    var hora = cells[1].innerText;
+    var patente = cells[2].innerText;
+    var nombre = cells[3].innerText;
+    var telefono = cells[4].innerText;
+    var email = cells[5].innerText;
+    var servicio = cells[6].innerText;
+
+    // Cargamos los datos en el formulario del modal
+    document.getElementById("fecha").value = fecha;
+    document.getElementById("hora").value = hora;
+    document.getElementById("patente").value = patente;
+    document.getElementById("nombre").value = nombre;
+    document.getElementById("telefono").value = telefono;
+    document.getElementById("email").value = email;
+
+    // Seleccionamos el servicio correspondiente
+    var servicios = document.getElementsByName("option");
+    for (var i = 0; i < servicios.length; i++) {
+        if (servicios[i].nextSibling.nodeValue.trim() === servicio) {
+            servicios[i].checked = true;
+            break;
+        }
+    }
+
+    // Almacenamos el ID del turno en el botón "Guardar cambios"
+    var btnGuardar = document.getElementById("btn-guardar");
+    btnGuardar.setAttribute("data-id", id);
+
+    // Mostramos el modal
+    var myModal = new bootstrap.Modal(document.getElementById('modalEditarTurno'), {
+        keyboard: false
+    });
+    myModal.show();
+}
+
+function guardarCambiosTest() {
+
+    // Obtenemos los valores del formulario del modal
+    const nombre = document.getElementById('nombre').value;
+    const telefono = document.getElementById('telefono').value;
+    const patente = document.getElementById('patente').value;
+    const email = document.getElementById('email').value;
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+    let servicio_descripcion = "";
+    let servicio_costo = "";
+
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+    if (selectedOption) {
+        const label = selectedOption.nextElementSibling.textContent;
+        const [servicio, costo] = label.split(' - ');
+
+        servicio_descripcion = servicio.trim();
+        servicio_costo = costo.replace('$', '').replace('.', '').trim();
+
+    }
+
+    const data = {
+        nombre_persona: nombre,
+        telefono_persona: telefono,
+        patente_vehiculo: patente,
+        mail_persona: email,
+        fecha: fecha,
+        hora: hora,
+        servicio_descripcion: servicio_descripcion,
+        servicio_costo: servicio_costo
+    };
+
+    fetch(BASEURL + '/api/crearTurno', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(data => {
+            Swal.fire({
+                icon: 'success',
+                title: '¡Formulario recibido!',
+                text: 'Gracias por agendar tu turno.',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar',
+            })
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al crear el turno',
+                text: 'Por favor, intente nuevamente',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar',
+            })
+        });
+
+    // Cerrar el modal después de guardar los cambios
+    var myModal = document.getElementById('modalEditarTurno');
+    var modal = bootstrap.Modal.getInstance(myModal);
+    modal.hide();
+}
+
+function guardarCambios() {
+    var id = document.getElementById("btn-guardar").getAttribute("data-id");
+
+    const nombre = document.getElementById('nombre').value.trim();
+    const telefono = document.getElementById('telefono').value.trim();
+    const patente = document.getElementById('patente').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+    let servicio_descripcion_value = "";
+    let servicio_costo_value = "";
+
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+    if (selectedOption) {
+        const label = selectedOption.nextElementSibling.textContent;
+        const [servicio, costo] = label.split(' - ');
+
+        servicio_descripcion_value = servicio.trim();
+        servicio_costo_value = costo.replace('$', '').replace('.', '').trim();
+
+    }
+
+    const data = {
+        nombre_persona: nombre,
+        telefono_persona: telefono,
+        patente_vehiculo: patente,
+        mail_persona: email,
+        fecha: fecha,
+        hora: hora,
+        servicio_descripcion: servicio_descripcion_value,
+        servicio_costo: servicio_costo_value
+    };
+
+    fetch(BASEURL + '/api/modificarTurno/' + id, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
